ea1723b974c1915f93cd36d2a8a92ebcebb3f04e
unknown <thakurusama72@gmail.com>
2023-07-30 01:05:46 +0530

    Added Drawer Navigator

---
+++ app/(drawer)/bookmarks.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const bookmarks = () => {
+  return (
+    <View>
+      <Text>bookmarks</Text>
+    </View>
+  )
+}
+
+export default bookmarks
---
+++ app/(drawer)/trending.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const trending = () => {
+  return (
+    <View>
+      <Text>trending</Text>
+    </View>
+  )
+}
+
+export default trending
